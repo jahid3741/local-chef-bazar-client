2f2bff1553ca0f2fc5b81aa29389fd7f6ee1e9a5
jahid3741 <mdj868445@gmail.com>
2026-05-18 20:26:23 +0600

    add the page of meal details full thing

--- src/Pages/MealDetails/MealDetails.jsx
+++ src/Pages/MealDetails/MealDetails.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
 import useAxiosSecure from "../../Hooks/UseAxiosSecure/UseAxiosSecure";
+import ReviewSection from "../../Components/ReviewSection/ReviewSection";
+import useAuth from "../../Hooks/UseAuth/UseAuth";
+import Swal from "sweetalert2";
 
 const MealDetails = () => {
   const { id } = useParams();
@@ -8,6 +11,7 @@ const MealDetails = () => {
   const axiosSecure = useAxiosSecure();
 
   const [meal, setMeal] = useState({});
+  const { user } = useAuth();
 
   useEffect(() => {
     axiosSecure.get(`/meals/${id}`).then((res) => {
@@ -27,7 +31,37 @@ const MealDetails = () => {
     estimatedDeliveryTime,
     chefExperience,
   } = meal;
+  const handleFavorite = async () => {
+    const favoriteData = {
+      userEmail: user?.email,
 
+      mealId: meal._id,
+
+      mealName: meal.foodName,
+
+      chefId: meal.chefId,
+
+      chefName: meal.chefName,
+
+      price: meal.price,
+    };
+
+    try {
+      const res = await axiosSecure.post("/favorites", favoriteData);
+
+      if (res.data.insertedId) {
+        Swal.fire({
+          icon: "success",
+          title: "Added to favorites!",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "warning",
+        title: error.response?.data?.message || "Already added",
+      });
+    }
+  };
   return (
     <div className="max-w-6xl mx-auto px-4 py-10">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
@@ -86,9 +120,19 @@ const MealDetails = () => {
             </div>
           </div>
 
-          <Link to={`/order/${meal._id}`} className="btn btn-primary mt-8">
-            Order Now
-          </Link>
+          <div className="flex items-center gap-4 mt-8">
+            <Link to={`/order/${meal._id}`} className="btn btn-primary">
+              Order Now
+            </Link>
+
+            <button
+              onClick={handleFavorite}
+              className="btn btn-outline btn-secondary"
+            >
+              Add To Favorite
+            </button>
+          </div>
+          <ReviewSection mealId={meal._id} />
         </div>
       </div>
     </div>
